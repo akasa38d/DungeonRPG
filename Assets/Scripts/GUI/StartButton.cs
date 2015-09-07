3ee2df2acd454600dfff98c69be2c23d5963b25b
akasa@38d <akasa38d@gmail.com>
2015-09-07 18:57:36 +0900

    グラフィック修正ほか

--- Assets/Scripts/GUI/StartButton.cs
+++ Assets/Scripts/GUI/StartButton.cs
@@ -5,6 +5,13 @@ public class StartButton : MonoBehaviour {
 
 	public void startGame()
 	{
+		StartCoroutine ("startGameCoroutine");
+	}
+
+	IEnumerator startGameCoroutine()
+	{
+		yield return new WaitForSeconds (1);
+
 		FadeManager.Instance.LoadLevel2(1, "Dungeon");
 	}
 }
